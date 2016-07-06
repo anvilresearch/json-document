acf88610b8703f8ed2d28d88de8bcd6d1f509440
Christian Smith <smith@anvil.io>
2016-07-05 21:22:56 -0700

    feat(BooleanValidation): initial class definition

---
+++ src/validation/BooleanValidation.js
@@ -0,0 +1,19 @@
+'use strict'
+
+/**
+ * Dependencies
+ * @ignore
+ */
+const Validation = require('./validation')
+
+/**
+ * BooleanValidation
+ */
+class BooleanValidation extends Validation {
+  type () {}
+}
+
+/**
+ * Export
+ */
+module.exports = BooleanValidation

7b0e9802912dc65894ede58073e9efc481ed6b3f
Christian Smith <smith@anvil.io>
2016-07-06 10:46:45 -0700

    refactor(Validation): move determineType method from JSONSchema class

--- src/validation/Validation.js
+++ src/validation/Validation.js
@@ -1,6 +1,19 @@
 'use strict'
 
 /**
+ * Types
+ */
+const TYPES = [
+  'array',
+  'boolean',
+  'integer',
+  'number',
+  'null',
+  'object',
+  'string'
+]
+
+/**
  * Validation
  *
  * @class
@@ -38,6 +51,79 @@ class Validation {
    */
   compile (schema, pointer) {
     throw new Error('compile must be implemented by subclasses')
+  }
+
+  /**
+   * Determine Type
+   *
+   * @description
+   * Identifies or infers the type of a given schema
+   *
+   * @param {Object} schema
+   * @returns {string}
+   */
+  static determineType (schema) {
+    if (!schema.type) {
+      // destructure keywords from schema for type inference
+      // we don't need number/integer keywords because we can't
+      // infer them
+      let {
+        type, minLength, maxLength, pattern, minItems, maxItems, items,
+        additionalItems, uniqueItems, properties, additionalProperties,
+        required, minProperties, maxProperties, dependencies,
+        patternProperties
+      } = schema
+
+      // check properties for indicators of the type
+      let isObject = Boolean(
+        properties ||
+        patternProperties ||
+        additionalProperties ||
+        minProperties ||
+        maxProperties ||
+        dependencies ||
+        // this last one will not work if we extend JSON Schema to allow
+        // `required` directly on non-object schemas
+        required
+      )
+
+      let isArray = Boolean(
+        minItems ||
+        maxItems ||
+        items ||
+        uniqueItems
+      )
+
+      let isString = Boolean(
+        minLength ||
+        maxLength ||
+        pattern
+      )
+
+      // If multiple types can be inferred, let the order of preference
+      // be "object", then "array", then "string".
+      //
+      // "null" and "boolean" cannot be inferred because they have no
+      // type-specific keywords
+      //
+      // "number" and "integer" cannot be inferred because they have
+      // ambiguous keywords
+      if (isObject) {
+        schema.type = 'object'
+      } else if (isArray) {
+        schema.type = 'array'
+      } else if (isString) {
+        schema.type = 'string'
+      } else {
+        throw new Error('Invalid schema definition')
+      }
+    }
+
+    else if (TYPES.indexOf(schema.type) === -1) {
+      throw new Error(`Can't determine type of ${JSON.stringify(schema)}`)
+    }
+
+    return schema.type
   }
 }
 

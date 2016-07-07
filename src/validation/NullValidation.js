be5c07dab0600cca3180b6db466b9dcb3f72e70d
Christian Smith <smith@anvil.io>
2016-07-06 20:58:52 -0700

    feat(NullValidation): compile and type validation methods

--- src/validation/NullValidation.js
+++ src/validation/NullValidation.js
@@ -10,7 +10,31 @@ const Validation = require('./validation')
  * NullValidation
  */
 class NullValidation extends Validation {
-  type () {}
+
+  /**
+   * Type
+   */
+  type (pointer) {
+    return `
+    if (value !== undefined && value !== null) {
+      validation.valid = false
+      validation.errors.push({
+        message: '"${pointer}" must be null'
+      })
+    }
+    `
+  }
+
+  /**
+   * Compile
+   */
+  compile (pointer, required) {
+    let block = ''
+    block += this.reference(pointer)
+    if (required) { block += this.required(pointer) }
+    block += this.type(pointer)
+    return block
+  }
 }
 
 /**

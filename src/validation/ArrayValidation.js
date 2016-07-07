b5cbb1178eaf1748bde9de01560163120a0d96e4
Christian Smith <smith@anvil.io>
2016-07-06 18:59:19 -0700

    feat(ArrayValidation): compile and type validation methods

--- src/validation/ArrayValidation.js
+++ src/validation/ArrayValidation.js
@@ -7,17 +7,85 @@
 const Validation = require('./Validation')
 
 /**
+ * Keywords
+ * @ignore
+ */
+const KEYWORDS = [
+  'additionalItems',
+  'enum',
+  'items',
+  'maxItems',
+  'minItems',
+  'uniqueItems',
+]
+
+/**
  * ArrayValidation
  * 4.4; 5.3
  */
 class ArrayValidation extends Validation {
-  type () {}
+
+  /**
+   * Type
+   */
+  type (pointer) {
+    return `
+    if (value !== undefined && !Array.isArray(value)) {
+      validation.valid = false
+      validation.errors.push({
+        message: '"${pointer}" must be an array'
+      })
+    }
+    `
+  }
+
+  /**
+   * Additional Items
+   */
   additionalItems () {}
+
+  /**
+   * Items
+   */
   items () {}
+
+  /**
+   * Max Items
+   */
   maxItems () {}
+
+  /**
+   * Min Items
+   */
   minItems () {}
+
+  /**
+   * Default Value
+   */
   defaultValue () {}
+
+  /**
+   * Unique Items
+   */
   uniqueItems () {}
+
+  /**
+   * Compile
+   */
+  compile (pointer, required) {
+    let {schema} = this
+    let block = ''
+
+    block += this.reference(pointer)
+
+    if (required) {
+      block += this.required(pointer)
+    }
+
+    block += this.type(pointer)
+
+    return block
+  }
 }
 
 /**

70f5cae2b91dfd7ada848cb0f5bab99ad1b11dc7
Christian Smith <smith@anvil.io>
2016-07-18 10:23:21 -0700

    refactor(Validator): incorporate all keywords into a single class

---
+++ src/Validator.js
@@ -0,0 +1,714 @@
+
+/**
+ * Formats
+ */
+const FORMATS = {
+  'date-time': /^\d\d\d\d-[0-1]\d-[0-3]\d[t\s][0-2]\d:[0-5]\d:[0-5]\d(?:\.\d+)?(?:z|[+-]\d\d:\d\d)$/i,
+
+  'uri': /^(?:[a-z][a-z0-9+-.]*)?(?:\:|\/)\/?[^\s]*$/i,
+
+  'email': /^[a-z0-9.!#$%&'*+\/=?^_`{|}~-]+@[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?(?:\.[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?)*$/i,
+
+  'ipv4': /^(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)$/,
+
+  'ipv6': /^\s*(?:(?:(?:[0-9a-f]{1,4}:){7}(?:[0-9a-f]{1,4}|:))|(?:(?:[0-9a-f]{1,4}:){6}(?::[0-9a-f]{1,4}|(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(?:(?:[0-9a-f]{1,4}:){5}(?:(?:(?::[0-9a-f]{1,4}){1,2})|:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3})|:))|(?:(?:[0-9a-f]{1,4}:){4}(?:(?:(?::[0-9a-f]{1,4}){1,3})|(?:(?::[0-9a-f]{1,4})?:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9a-f]{1,4}:){3}(?:(?:(?::[0-9a-f]{1,4}){1,4})|(?:(?::[0-9a-f]{1,4}){0,2}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9a-f]{1,4}:){2}(?:(?:(?::[0-9a-f]{1,4}){1,5})|(?:(?::[0-9a-f]{1,4}){0,3}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?:(?:[0-9a-f]{1,4}:){1}(?:(?:(?::[0-9a-f]{1,4}){1,6})|(?:(?::[0-9a-f]{1,4}){0,4}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:))|(?::(?:(?:(?::[0-9a-f]{1,4}){1,7})|(?:(?::[0-9a-f]{1,4}){0,5}:(?:(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(?:\.(?:25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}))|:)))(?:%.+)?\s*$/i,
+
+  'hostname': /^[a-z](?:(?:[-0-9a-z]{0,61})?[0-9a-z])?(\.[a-z](?:(?:[-0-9a-z]{0,61})?[0-9a-z])?)*$/i,
+
+}
+
+/**
+ * Validator
+ *
+ * Compile an object describing a JSON Schema into a validation function.
+ */
+class Validator {
+
+  /**
+   * Compile (static)
+   *
+   * @description
+   * Compile an object describing a JSON Schema into a validation function.
+   *
+   * @param {Object} schema
+   * @returns {Function}
+   */
+  static compile (schema) {
+    let validator = new Validator(schema)
+
+    let body = `
+      let value
+      let valid = true
+      let errors = []
+      ${ validator.compile() }
+      return { valid, errors }
+    `
+
+    console.log(body)
+    return new Function('data', body)
+  }
+
+  /**
+   * Constructor
+   *
+   * @param {Object} schema - object representation of a schema
+   * @param {string} address - reference to ancestry of schema param
+   */
+  constructor (schema, address) {
+    this.schema = schema
+    this.address = address
+  }
+
+  /**
+   * Compile
+   *
+   * @description
+   * The instance compile method is "dumb". It only sequences invocation of more
+   * specific compilation methods. It generates code to
+   *
+   *  - reference a value from input
+   *  - validate type(s) of input
+   *  - validate constraints described by various schema keywords
+   *
+   * Conditional logic related to code generation is pushed downsteam to type-
+   * specific methods.
+   */
+  compile (required) {
+    let block = ``
+
+    block += this.reference()
+
+    if (required) {
+      block += this.required()
+    }
+
+    // type validation
+    block += this.type()
+
+    // type specific validation generators
+    // null and boolean are covered by this.type()
+    // integer should be covered by number and this.type()
+    block += this.array()
+    block += this.number()
+    block += this.object()
+    block += this.string()
+
+
+    // TODO invoke generic methods
+    //block += this.enum()
+
+
+    return block
+  }
+
+  /**
+   * Reference
+   */
+  reference () {
+    let {address} = this
+    let chain = address && address.split('.')
+
+    let block = `
+      // NEXT VALUE
+      value = data `
+
+    chain && chain.forEach((token, index) => {
+      let reference = `data.${ chain.slice(0, index + 1).join('.') }`
+      block += `&& ${reference}`
+    })
+
+    block += `
+    `
+
+    return block
+  }
+
+  /**
+   * Type
+   *
+   * @description
+   * Generate code to validate type(s) of input values
+   */
+  type () {
+    let {schema:{type}} = this
+    let block = ``
+
+    if (type) {
+      let types = (Array.isArray(type)) ? type : [type]
+      let conditions = types.map(type => {
+        // TODO: can we make a mapping object for this to clean it up?
+        if (type === 'array') return `!Array.isArray(value)`
+        if (type === 'boolean') return `typeof value !== 'boolean'`
+        if (type === 'integer') return `!Number.isInteger(value)`
+        if (type === 'null') return `value !== null`
+        if (type === 'number') return `typeof value !== 'number'`
+        if (type === 'object') return `(typeof value !== 'object' || Array.isArray(value) || value === null)`
+        if (type === 'string') return `typeof value !== 'string'`
+      }).join(' && ')
+
+      block += `
+      // type checking
+      if (value !== undefined && ${conditions}) {
+        valid = false
+        errors.push({ message: 'invalid type' })
+        return { valid, errors }
+      }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * Array
+   */
+  array () {
+    let keywords = [
+      'additionalItems', 'items', 'minItems', 'maxItems', 'uniqueItems', 'enum'
+    ]
+    let validations = this.validations(keywords)
+    let block = ``
+
+    if (validations.length > 0) {
+      block += `
+      // array validations
+      if (Array.isArray(value)) {
+      ${validations}
+      }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * Number
+   */
+  number () {
+    let keywords = ['minimum', 'maximum', 'multipleOf', 'enum']
+    let validations = this.validations(keywords)
+    let block = ``
+
+    if (validations.length > 0) {
+      block += `
+      // number validations
+      if (typeof value === 'number') {
+      ${validations}
+      }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * Object
+   */
+  object () {
+    let keywords = [
+      'maxProperties', 'minProperties', 'additionalProperties',
+      'properties', 'patternProperties', 'dependencies', 'schemaDependencies',
+      'propertyDependencies', 'enum'
+    ]
+    let validations = this.validations(keywords)
+    let block = ``
+
+    if (validations.length > 0) {
+      block += `
+      // object validations
+      if (typeof value === 'object' && value !== null) {
+      ${validations}
+      }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * String
+   *
+   * @description
+   * String-specific validation code generation
+   */
+  string () {
+    let keywords = ['maxLength', 'minLength', 'pattern', 'format', 'enum']
+    let validations = this.validations(keywords)
+    let block = ``
+
+    if (validations.length > 0) {
+      block += `
+      // string validations
+      if (typeof value === 'string') {
+      ${validations}
+      }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * Validations
+   *
+   * @description
+   * Iterate over keywords and invoke code generator methods for each.
+   * Concatenate the results together and return. Used by "type" methods
+   * such as this.array() and this.string()
+   *
+   * @param {Array} keywords
+   * @returns string
+   */
+  validations (keywords) {
+    let {schema} = this
+    let block = ``
+
+    let constraints = Object.keys(schema).filter(key => {
+      return keywords.indexOf(key) !== -1
+    })
+
+    constraints.forEach(keyword => {
+      block += this[keyword]()
+    })
+
+    return block
+  }
+
+  /**
+   * Generic Validations
+   */
+
+  /**
+   * Enum
+   */
+  enum () {
+    let {schema:{enum: enumerated}} = this
+    let block = ``
+
+    if (enumerated) {
+      block += `
+      // validate enum
+      if (${JSON.stringify(enumerated)}.indexOf(value) === -1) {
+        valid = false
+        errors.push({ message: value + ' is not an accepted value' })
+      }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * anyOf
+   */
+  anyOf () {
+    return ``
+  }
+
+  /**
+   * allOf
+   */
+  allOf () {
+    return ``
+  }
+
+  /**
+   * oneOf
+   */
+  oneOf () {
+    return ``
+  }
+
+  /**
+   * Not
+   */
+  not () {
+    return ``
+  }
+
+  /**
+   * Definitions
+   */
+  definitions () {
+    return ``
+  }
+
+  /**
+   * Object Validations
+   */
+
+  /**
+   * Properties
+   */
+  properties () {
+    let {schema} = this
+    let {properties,required} = schema
+    let block = ``
+
+    // ensure the value of "required" schema property is an array
+    required = (Array.isArray(required) ? required : [])
+
+    if (typeof properties === 'object') {
+      Object.keys(properties).forEach(key => {
+        let subschema = properties[key]
+        let isRequired = required.indexOf(key) !== -1
+        let validation = new Validator(subschema, key)
+        block += validation.compile(isRequired)
+      })
+    }
+
+    return block
+  }
+
+  /**
+   * patternProperties
+   */
+  patternProperties () {
+    let {schema} = this
+    let {patternProperties} = schema
+    let block = ``
+
+    if (typeof patternProperties === 'object') {
+      Object.keys(patternProperties).forEach(key => {
+        // TODO
+        // how do we make validation code that will apply to matching property names
+        // that are unknown before-hand?
+      })
+    }
+
+    return block
+  }
+
+  /**
+   * additionalProperties
+   */
+  additionalProperties () {
+    let {schema} = this
+    let {properties,additionalProperties} = schema
+    let block = ``
+
+    let knownProperties = Object.keys(properties).map(key => {
+      return `key !== '${key}'`
+    })
+
+    let conditions = [
+      'data.hasOwnProperty(key)'
+    ].concat(knownProperties).join(' && ')
+
+    if (additionalProperties) {
+      block += `
+      // additionalProperties
+      for (let key in data) {
+        if (${conditions}) {
+          // we need to reference, check type, and apply other validations
+          // works differently that these others because we don't know in
+          // advance what the keys are to reference
+          value = data[key]
+
+          // how to get type?
+          // is there some way we can create a new compiler for a generalized ref?
+          //
+          // TODO Feels like this needs talking through
+        }
+      }
+      `
+    }
+
+
+    return block
+  }
+
+  /**
+   * minProperties
+   */
+  minProperties () {
+    let {schema:{minProperties}} = this
+
+    return `
+        // min properties
+        if (Object.keys(value).length < ${minProperties}) {
+          valid = false
+          errors.push({
+            message: 'too few properties'
+          })
+        }
+    `
+  }
+
+  /**
+   * maxProperties
+   */
+  maxProperties () {
+    let {schema:{maxProperties}} = this
+
+    return `
+        // max properties
+        if (Object.keys(value).length > ${maxProperties}) {
+          valid = false
+          errors.push({
+            message: 'too many properties'
+          })
+        }
+    `
+  }
+
+  /**
+   * Dependencies
+   */
+  dependencies () {
+    return ``
+  }
+
+  /**
+   * Required
+   *
+   *
+   * TODO: THIS IS WRONG; SHOULD BE CALLED FROM this.object()
+   */
+  required () {
+    let {schema:{properties}} = this
+    let block = ``
+
+    block += `
+      // omfg it's missing!!
+      if (value === undefined) {
+        valid = false
+        errors.push({ message: 'is required' })
+      }
+    `
+
+    return block
+    //let {schema:{required}} = this
+    //let block = `
+    //  // required
+    //`
+    //if (Array.isArray(required)) {
+    //  required.forEach(key => {
+    //    block += `
+    //    // ${key} is required
+    //    if (value === undefined) {
+    //      valid = false
+    //      errors.push({ message: '"${key}" is required' })
+    //    }
+    //    `
+    //  })
+    //}
+
+    //return block
+  }
+
+  /**
+   * Array Validations
+   */
+  additionalItems () {
+    let {schema:{items,additionalItems}} = this
+    let block = ``
+
+    if (additionalItems === false && Array.isArray(items)) {
+      block += `
+        // don't allow additional items
+        if (value.length > ${items.length}) {
+          valid = false
+          errors.push({ message: 'additional items not allowed' })
+        }
+      `
+    }
+
+    if (Array.isArray(items)) {
+      block += `
+        // additional items
+        for (var i = ${items.length}; i < value.length; i++) {
+          // TODO
+          // create a new validation object and recusrse
+          // we'll need to modify this.reference() to accommodate arrays
+          // and deal with restoring the referece to parent for subsequent
+          // validations of that property
+        }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * Items
+   */
+  items () {
+    return ``
+  }
+
+  /**
+   * minItems
+   */
+  minItems () {
+    let {schema:{minItems}} = this
+
+    return `
+        // min items
+        if (value.length < ${minItems}) {
+          valid = false
+          errors.push({
+            message: 'too few properties'
+          })
+        }
+    `
+  }
+
+  /**
+   * maxItems
+   */
+  maxItems () {
+    let {schema:{maxItems}} = this
+
+    return `
+        // max items
+        if (value.length > ${maxItems}) {
+          valid = false
+          errors.push({ message: 'too many properties' })
+        }
+    `
+  }
+
+  /**
+   * uniqueItems
+   *
+   * TODO
+   * optimize
+   */
+  uniqueItems () {
+    let {schema:{uniqueItems}} = this
+    let block = ``
+
+    if (uniqueItems === true) {
+      block += `
+        // validate unique items
+        let values = value.map(v => JSON.stringify(v)) // TODO: optimize
+        let set = new Set(values)
+        if (values.length !== set.size) {
+          valid = false
+          errors.push({ message: 'items must be unique' })
+        }
+      `
+    }
+
+    return block
+  }
+
+  /**
+   * String Validations
+   */
+
+  /**
+   * minLength
+   */
+  minLength () {
+    let {schema:{minLength}} = this
+
+    return `
+        // validate minLength
+        if (Array.from(value).length < ${minLength}) {
+          valid = false
+          errors.push({ message: 'too short' })
+        }
+    `
+  }
+
+  /**
+   * maxLength
+   */
+  maxLength () {
+    let {schema:{maxLength}} = this
+
+    return `
+        // validate maxLength
+        if (Array.from(value).length > ${maxLength}) {
+          valid = false
+          errors.push({ message: 'too long' })
+        }
+    `
+  }
+
+  /**
+   * Pattern
+   */
+  pattern () {
+    let {schema:{pattern}} = this
+
+    if (pattern) {
+      return `
+          // validate pattern
+          if (!value.match(new RegExp('${pattern}'))) {
+            valid = false
+            errors.push({ message: 'does not match the required pattern' })
+          }
+      `
+    }
+  }
+
+  /**
+   * Format
+   */
+  format () {
+    let {schema:{format}} = this
+    let matcher = FORMATS[format]
+
+    if (matcher) {
+      return `
+      if (!value.match(${matcher})) {
+        valid = false
+        errors.push({ message: 'is not "${format}" format' })
+      }
+      `
+    }
+  }
+
+  /**
+   * Number|Integer Validations
+   */
+
+  /**
+   * Minimum
+   */
+  minimum () {
+    let {schema:{minimum,exclusiveMinimum}} = this
+    let operator = exclusiveMinimum === true ? '<=' : '<'
+
+    return `
+        if (value ${operator} ${minimum}) {
+          valid = false
+          errors.push({ message: 'too small' })
+        }
+    `
+  }
+
+  /**
+   * Maximum
+   */
+  maximum () {
+    let {schema:{maximum,exclusiveMaximum}} = this
+    let operator = exclusiveMaximum === true ? '>=' : '>'
+
+    return `
+        if (value ${operator} ${maximum}) {
+          valid = false
+          errors.push({ message: 'too large' })
+        }
+    `
+  }
+
+  /**
+   * multipleOf
+   */
+  multipleOf () {
+    let {schema:{multipleOf}} = this
+
+    return `
+        if (value % ${multipleOf} !== 0) {
+          valid = false
+          errors.push({ message: 'must be a multiple of ${multipleOf}' })
+        }
+    `
+  }
+}
+
+module.exports = Validator
---
+++ test/JSONSchemaTestSuite.js
@@ -0,0 +1,215 @@
+'use strict'
+
+/**
+ * Test dependencies
+ */
+const assert = require('assert')
+const suite = require('json-schema-test-suite')
+
+/**
+ * Code under test
+ */
+//const JSONSchema = require('../src/JSONSchema')
+const Validator = require('../src/Validator')
+
+function JSONSchemaFactory (schema, options) {
+  if (typeof schema === 'string') {
+    schema = JSON.parse(text)
+  }
+
+  return {
+    validate: function (json) {
+      try {
+        let fn = Validator.compile(schema)
+        return fn(json)
+        //let compiled = new JSONSchema(schema)
+        //return compiled.validate(json)
+      } catch (err) {
+        console.log(err)
+        return { valid: false, errors: [err.message] }
+      }
+    }
+  }
+}
+
+/**
+ * Test runner
+ */
+run(JSONSchemaFactory, 'draft4', {
+  name: 'feldspar',
+  draft: 'draft4',
+  results: {
+    pass: 0,
+    fail: 0
+  }
+})
+
+
+/**
+ * Run Tests
+ */
+function run (factory, draft, validatorResult) {
+  describe.only(draft, () => {
+    let tests = suite.testSync(factory, {}, void 0, draft)
+
+    tests.forEach(test => {
+      let {name, schemas} = test
+
+      describe(name, () => {
+        schemas.forEach(schema => {
+          let {description} = schema
+
+          describe(description, () => {
+            schema.tests.forEach(assertion => {
+
+              it(assertion.description, () => {
+                let result = assertion.result
+
+                if (result.valid === assertion.valid) {
+                  //console.log('SCHEMA', schema.schema)
+                  //console.log('DATA', assertion.data)
+                  //console.log('EXPECTED', assertion.valid)
+                  //console.log('RESULT', result)
+                  validatorResult.results.pass++
+                } else {
+                  console.log('SCHEMA', schema.schema)
+                  console.log('DATA', assertion.data)
+                  console.log('EXPECTED', assertion.valid)
+                  console.log('RESULT', result)
+                  validatorResult.results.fail++
+                }
+
+                assert.equal(result.valid, assertion.valid)
+              })
+            })
+          })
+        })
+      })
+    })
+  })
+}
+
+/**
+ * Test Dependencies
+ */
+//const assert = require('assert')
+//const glob = require('glob')
+//const util = require('util')
+//const suite = require('json-schema-test-suite')
+
+/**
+ * Drafts
+ */
+//const drafts = [ [>'draft3',<] 'draft4' ]
+
+/**
+ * Verify Suite
+ */
+//describe('verify test suite loads all json test files', function () {
+//  let testMessage = 'The number of %s test groups should match the number of %s json files'
+//  let errorMessage = 'the actual number of test groups was expected match the number of %s json files'
+
+//  let allPattern = './tests/%s/**/*.json'
+//  let requiredPattern = { glob: './tests/%s/**/*.json', ignore: './tests/%s/optional/*.json' }
+//  let optionalPattern = './tests/%s/optional/*.json'
+//  let minPattern = './tests/%s/**/min*.json'
+
+//  let testPlans = [
+//    { name: 'all', globPattern: allPattern },
+//    { name: 'required', globPattern: requiredPattern, filter: suite.requiredOnlyFilter },
+//    { name: 'optional', globPattern: optionalPattern, filter: suite.optionalOnlyFilter },
+//    { name: '"min"-prefixed', globPattern: minPattern, filter: function (file) { return /^min/.test(file) } }
+//  ]
+
+//  function loadFiles(draft, globPattern) {
+//    if (typeof globPattern == 'string') {
+//      return glob.sync(util.format(globPattern, draft))
+//    } else {
+//      return glob.sync(util.format(globPattern.glob, draft), {
+//        ignore: util.format(globPattern.ignore, draft)
+//      })
+//    }
+//  }
+
+//  function compareCount(draft, globPattern, filter, name) {
+//    console.log('COMPARE COUNT', draft, globPattern, filter, name)
+//    let tests = suite.loadSync({ filter: filter, draft: draft })
+//    let files = loadFiles(draft, globPattern)
+
+//    assert.equal(tests.length, files.length, util.format(errorMessage, name))
+//  }
+
+//  // for the combination of draft directories and test plans, create a test case
+//  // and verify the number of tests returned by the test suite is equal to the actual
+//  // number of files that match the glob pattern.
+//  drafts.forEach(function (draft) {
+//    testPlans.forEach(function (pt) {
+//      it(util.format(testMessage, draft, pt.name), function () {
+//        compareCount(draft, pt.globPattern, pt.filter, pt.name)
+//      })
+//    })
+//  })
+
+//  // test helper functions loadRequiredSync and loadOptionalSync
+//  drafts.forEach(function (draft) {
+//    it(util.format('should load required %s tests', draft), function () {
+//      let tests = suite.loadRequiredSync(draft)
+//      let files = loadFiles(draft, requiredPattern)
+
+//      assert.equal(tests.length, files.length, util.format(errorMessage, 'required'))
+//    })
+
+//    it(util.format('should load optional %s tests', draft), function () {
+//      let tests = suite.loadOptionalSync(draft)
+//      let files = loadFiles(draft, optionalPattern)
+
+//      assert.equal(tests.length, files.length, util.format(errorMessage, 'optional'))
+//    })
+//  })
+
+//})
+
+/**
+ * Validator Tests
+ */
+//describe('validator tests', function() {
+//  let validatorResults = []
+
+//  after(function() {
+//    validatorResults.forEach(function(validator) {
+//      console.log('\n******************************')
+//      console.log('validator: %s (%s)', validator.name, validator.draft)
+//      console.log('pass: ' + validator.results.pass)
+//      console.log('fail: ' + validator.results.fail)
+//    })
+//  })
+
+  /**
+   * IMPLEMENTATION TESTS
+   */
+//  describe('feldspar validator tests', function () {
+//    let JSONSchema = require('../src/JSONSchema')
+
+//    function JSONSchemaFactory (schema, options) {
+//      if (typeof schema === 'string') {
+//        schema = JSON.parse(text)
+//      }
+
+//      return {
+//        validate: function (json) {
+//          try {
+//            return JSONSchema.validate(json, schema)
+//          } catch (err) {
+//            return { valid: false, errors: [err.message] }
+//          }
+//        }
+//      }
+//    }
+
+//    drafts.forEach(draft => {
+//      let result = { name: 'feldspar', draft, results: { pass: 0, fail: 0 }}
+//      validatorResults.push(result)
+//      //runTests(JSONSchemaFactory, draft, result)
+//    })
+//  })
+//})

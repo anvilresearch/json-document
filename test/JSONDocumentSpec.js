1a10cdc6683d409cdee92fb05bfdb59d8d58e47f
Christian Smith <smith@anvil.io>
2016-07-24 22:17:40 -0700

    test(JSONDocument): schema getter, constructor, initialize, validate methods

--- test/JSONDocumentSpec.js
+++ test/JSONDocumentSpec.js
@@ -25,32 +25,142 @@ const JSONSchema = require(path.join(cwd, 'src', 'JSONSchema'))
 /**
  * Tests
  */
-describe('JSONDocument', () => {
+describe.only('JSONDocument', () => {
 
+  /**
+   * Schema
+   */
+  describe('static get schema', () => {
+    it('should throw an error', () => {
+      expect(() => {
+        JSONDocument.schema
+      }).to.throw('Schema must be defined by classes extending JSONDocument')
+    })
+  })
+
+  /**
+   * Constructor
+   */
   describe('constructor', () => {
-    let schema, instance
+    let doc, Doc, data, options
+
+    beforeEach(() => {
+      Doc = class extends JSONDocument {}
+      sinon.stub(Doc.prototype, 'initialize')
+
+      data = {}
+      options = {}
+      doc = new Doc(data, options)
+    })
+
+    afterEach(() => {
+      Doc.prototype.initialize.restore()
+    })
+
+    it('should initialize instance', () => {
+      doc.initialize.should.have.been.calledWith(data, options)
+    })
+  })
+
+  /**
+   * Initialize
+   */
+  describe('initialize', () => {
+    let schema, doc, Doc, data, options
 
     before(() => {
-      let schema = new JSONSchema({
-        properties: {
-          foo: { type: 'string', default: 'foo' },
-          bar: { type: 'string', default: 'bar' }
-        }
-      })
+      schema = { initialize: sinon.spy() }
 
-      class MyDoc extends JSONDocument {
+      Doc = class extends JSONDocument {
         static get schema () {
           return schema
         }
       }
 
-      instance = new MyDoc()
+      doc = new Doc()
+
+      data = {}
+      options = {}
+      doc.initialize(data, options)
     })
 
-    it('should initialize from the schema', () => {
-      instance.foo.should.equal('foo')
-      instance.bar.should.equal('bar')
+    it('should initialize instance with schema', () => {
+      schema.initialize.should.have.been.calledWith(data, options)
     })
   })
 
+  /**
+   * Validate
+   */
+  describe('validate', () => {
+    let schema, alternate, doc, Doc, data, options
+
+    beforeEach(() => {
+      schema = {
+        initialize: sinon.spy(),
+        validate: sinon.spy()
+      }
+
+      alternate = {
+        initialize: sinon.spy(),
+        validate: sinon.spy()
+      }
+
+      Doc = class extends JSONDocument {
+        static get schema () {
+          return schema
+        }
+      }
+
+      doc = new Doc()
+    })
+
+    it('should validate instance with defined schema', () => {
+      doc.validate()
+      schema.validate.should.have.been.calledWith(doc)
+    })
+
+    it('should validate instance with schema argument', () => {
+      doc.validate(alternate)
+      schema.validate.should.not.have.been.called
+      alternate.validate.should.have.been.calledWith(doc)
+    })
+  })
+
+  /**
+   * Patch
+   */
+  describe('patch', () => {
+    it('should apply patch to instance')
+  })
+
+  /**
+   * Select
+   */
+  describe('select', () => {
+    it('should filter object properties')
+  })
+
+  /**
+   * Project
+   */
+  describe('project', () => {
+    it('should project from an instance of Mapping')
+    it('should project from a mapping description')
+    it('should project from a mapping name')
+  })
+
+  /**
+   * Serialize
+   */
+  describe('static serialize', () => {
+    it('should stringify value to JSON')
+  })
+
+  /**
+   * Deserialize
+   */
+  describe('static deserialize', () => {
+    it('should parse JSON')
+  })
 })

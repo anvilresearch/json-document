4343c4608c71b8b78be1c2127899ad6c847ff660
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-10-11 13:00:29 -0400

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+var path = require('path')
+
+module.exports = {
+  entry: [
+    './src/index.js'
+  ],
+  output: {
+    path: path.join(__dirname, '/dist/'),
+    filename: 'json-document.min.js',
+    library: 'JSONDocument',
+    libraryTarget: 'var'
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015']
+        }
+      },
+      {
+        test: /\.json$/,
+        loader: 'json'
+      }
+    ]
+  },
+  node: {
+    fs: 'empty'
+  },
+  devtool: 'source-map'
+}

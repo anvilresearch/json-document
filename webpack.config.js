35fd9708b829749aca7fd4f1254b1a75e4b2bced
Dmitri Zagidulin <dzagidulin@gmail.com>
2016-10-11 13:25:47 -0400

    Remove extraneous directives

--- webpack.config.js
+++ webpack.config.js
@@ -13,21 +13,10 @@ module.exports = {
   module: {
     loaders: [
       {
-        test: /\.js$/,
-        exclude: /(node_modules)/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['es2015']
-        }
-      },
-      {
         test: /\.json$/,
         loader: 'json'
       }
     ]
-  },
-  node: {
-    fs: 'empty'
   },
   devtool: 'source-map'
 }
